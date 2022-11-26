d4ecef752009a8b7b728341482bc6e206f8f628c
daveads <the.daveads@gmail.com>
2022-11-26 13:42:48 +0100

    accept button

--- src/core/userverify.py
+++ src/core/userverify.py
@@ -1,14 +1,17 @@
 import discord
+from src.core.config_parser import BotConfigs
+
+bot_configs = BotConfigs()
 
 class UserVerify(discord.ui.View):
     def __init__(self, bot):
         super().__init__(timeout=None)
-
         self.bot = bot
 
     @discord.ui.button(label='Accept', style=discord.ButtonStyle.green, custom_id='accept')
     async def accept(self, interaction: discord.Interaction, button: discord.ui.Button):
 
+        roleObj = discord.utils.get(interaction.guild.roles, id=bot_configs.roles("self_ver"))
 
         embeds = interaction.message.embeds[0].to_dict()
         
@@ -16,8 +19,11 @@ class UserVerify(discord.ui.View):
         print("user to give role id", user_id)
 
         self.user = await interaction.guild.fetch_member(user_id)
+        
+        # add the verifiy role
+        await self.user.add_roles(roleObj)
+    
         await self.user.send("verification accepted")
-
         await interaction.response.defer()
         await interaction.delete_original_response()
         await interaction.channel.purge(limit=1)
