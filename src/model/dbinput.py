4bff0e259e321292ef4621dde0811e5a3cf9341a
daveads <the.daveads@gmail.com>
2023-01-16 12:01:33 +0100

    model

---
+++ src/model/dbinput.py
@@ -0,0 +1,34 @@
+import sqlite3
+from datetime import datetime
+
+con = sqlite3.connect("verification.db")
+cur = con.cursor()
+
+class profile_data():
+  def __init__(self, username, user_id, age_verified, age_verified_g, selfie_verified, selfie_verified_g,  age_verification_date,  selfie_verification_date):
+    self.username = username
+    self.user_id = user_id
+    self.age_verified = age_verified
+    self.age_verified_g = age_verified_g
+    self.selfie_verified = selfie_verified
+    self.selfie_verified_g = selfie_verified_g
+    self.age_verification_date = age_verification_date
+    self.selfie_verification_date = selfie_verification_date
+    #datetime.utcnow().strftime("%d-%m-%Y")
+
+    try:
+      cur.execute("INSERT INTO verifi VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)", (
+      self.username,
+      self.user_id,
+      self.age_verified,
+      self.age_verified_g,
+      self.selfie_verified,
+      self.selfie_verified_g,
+      self.age_verification_date,
+      self.selfie_verification_date
+    ))
+      con.commit()
+      con.close()
+
+    except sqlite3.Error as error:
+      print(f"unable to create add {self.username} into the database >> ", error)
---
+++ src/model/queries.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+class verifiedque():
+
+    def __init__(self):
+
+        self.con = sqlite3.connect("verification.db")
+        self.cur = self.con.cursor()
+
+
+    def get_user(self, id):
+        try:
+            self.cur.execute(f"SELECT * FROM verifi WHERE username_id='{id}'")
+            user_in_db= self.cur.fetchone();
+            return user_in_db
+
+        except sqlite3.Error as error:
+            print("error error queries >>", error)
+
+
+    def update(self, table, value, username_id):
+        try:
+            self.cur.execute(f"UPDATE verifi SET {table} ='{value}' WHERE username_id='{username_id}'")
+
+        except sqlite3.Error as error:
+            print(f"unable to update user {table} >>", error)
